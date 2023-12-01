8833ce2bd06bc42c8af908b687dbba38d62ac49c
Kaviru Hapuarachchi <hapuarachchikaviru@gmail.com>
2023-12-01 21:18:03 +0530

    load issue

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -8,10 +8,22 @@ function Register() {
     const [pageLoaded, setPageLoaded] = useState(false);
 
     useEffect(() => {
-        const timeout = setTimeout(() => {
+        const script = document.createElement("script");
+        script.src = "//embed.typeform.com/next/embed.js";
+        script.async = true;
+
+        const handleScriptLoad = () => {
             setPageLoaded(true);
-        }, 500);
-        return () => clearTimeout(timeout);
+        };
+
+        script.onload = handleScriptLoad;
+
+        document.body.appendChild(script);
+
+        return () => {
+            // Clean up script on component unmount if needed
+            document.body.removeChild(script);
+        };
     }, []);
 
     return (
@@ -32,23 +44,16 @@ function Register() {
                         Register for the event by filling out the form below.
                     </p>
 
-
-
                     <div className='mx-4 md:mx-28'>
-                        <p className='text-center mb-10 text-slate-400'>Takes a few seconds to load. If not please <Link href="https://www.acm.ko-de.org/"><span className=' text-blue-700'>click here.</span> </Link></p>
+                        <p className='text-center mb-10 text-slate-400'>Takes a few seconds to load. If not, please <Link href="#" as="/register" passHref><span className=' text-blue-700'>click here.</span> </Link></p>
                         <div className="tf-live-embed mb-10 mx-4 md:mx-28">
-
                             <div data-tf-live="01HFYHP17PJQC6AYQ1YBDPDB25"></div>
-                            <script async src="//embed.typeform.com/next/embed.js"></script>
                         </div>
                     </div>
-
 
                 </div>
                 <Footer />
             </div>
-
-
         </>
     );
 }
