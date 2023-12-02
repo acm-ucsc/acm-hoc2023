3d02199bdbc6ad9ceccfe9aec39081481657b6ac
Kaviru Hapuarachchi <hapuarachchikaviru@gmail.com>
2023-12-02 16:31:04 +0530

    added google form

--- src/pages/registertwo.jsx
+++ src/pages/registertwo.jsx
@@ -46,11 +46,8 @@ function Register() {
                         Register for the event by filling out the form below.
                     </p>
 
-                    <div className='mx-4 md:mx-28'>
-                        <p className='text-center mb-10 text-slate-400'>Takes a few seconds to load. If not, please <Link href="/registertwo"><span className=' text-blue-700'>click here.</span> </Link></p>
-                        <div className="tf-live-embed mb-10 mx-4 md:mx-28">
-                            <div data-tf-live="01HFYHP17PJQC6AYQ1YBDPDB25"></div>
-                        </div>
+                    <div className='mx-4 md:mx-28 flex flex-col items-center'>
+                        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdl3JgJMr5YrB0xsbi7VrN3jafI1XdHT2iE5jGNU6c-JkYtJA/viewform?embedded=true" width="640" height="1596" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
                     </div>
 
                 </div>
